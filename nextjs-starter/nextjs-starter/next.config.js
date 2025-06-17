6c0873058b7e48f72ed1255c9b04f4711caa5fce
Shan D. Wanigasuriya <shandilhara97@gmail.com>
2025-06-17 18:38:57 +0530

    Test

--- nextjs-starter/nextjs-starter/next.config.js
+++ nextjs-starter/nextjs-starter/next.config.js
@@ -53,6 +53,11 @@ const nextConfig = {
         hostname: 'feaas*.blob.core.windows.net',
         port: '',
       },
+      {
+        protocol: 'https',
+        hostname: 'feature-testing-nine.vercel.app',
+        port: '',
+      },
     ]
   },
 
